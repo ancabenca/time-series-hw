85463ccc6225120209673ded7d69ec28bdfdc113
DESKTOP-6VS722T\Uzivatel <benesova.work@gmail.com>
2024-03-24 20:49:50 +0100

    update: functioning loading of data as well as basic analysis - minor changes in the data set

--- Assigment1/Code/R project/data-loading.R
+++ Assigment1/Code/R project/data-loading.R
@@ -1,9 +1,57 @@
 #This part is for data loading and basic data exploration
 #ABE: Please do not delete or change mindlessly lines or we get nasty conflicts in GIT
 
-##Libraries--------------------------------------------------------------
+# CleaningWS ------------------------------------------------------------------
+rm(list = ls())
+graphics.off()
+#ABE: do not forgo to set directory to the correct path!
+
+#Libraries--------------------------------------------------------------
+library(tseries)
+library(forecast)
+library(quantmod)
+library(lmtest)
+library(car)
+library(openxlsx)
+#install.packages('openxlsx')
+
+#Data loading-----------------------------------------------------------
+hungary_data <- t(read.xlsx("Data/une_rt_m.xlsx", sheet = "Hungary",na.strings = ":"))#transpose, so we can use ts fun properly
+colnames(hungary_data) <- c("hungary")
+hungary_data <- apply(hungary_data, 2, as.numeric)
+hungary_data <- ts(hungary_data, frequency = 12, start = c(1983, 1))
 
 
-##Data loading-----------------------------------------------------------
+#in-sample
+hungary_data.in <- window(hungary_data, start = c(2010, 1), end = c(2022, 12))
 
-##Data exploration-------------------------------------------------------
+#out-sample
+hungary_data.out <- window(hungary_data, start = c(2023, 1), end = c(2023, 12))
+
+#Check
+start(hungary_data)
+end(hungary_data)
+frequency(hungary_data)
+
+# Check for NA values in the dataset
+na_values <- is.na(hungary_data)
+na_values <- is.na(hungary_data.in) #no NA in our in-sample
+print(which(na_values, arr.ind = TRUE))
+
+#Data exploration-------------------------------------------------------
+View(hungary_data)
+View(hungary_data.in)
+View(hungary_data.out)
+
+summary(hungary_data)
+hist(hungary_data)
+plot(hungary_data)
+
+#Data visualization-------------------------
+autoplot(hungary_data)
+ggseasonplot(hungary_data) #seasonal months
+ggsubseriesplot(hungary_data)
+
+Acf(hungary_data)
+plot(decompose(hungary_data.in))
+plot(diff(hungary_data.in)) #fluctuation of unemployemnt
