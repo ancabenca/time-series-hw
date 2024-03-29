f786fecd791b763cf393221f0f1bbc2576ef3798
Albcraft17 <zejdalbert@seznam.cz>
2024-03-29 14:05:38 +0100

    no message

--- Assigment1/Code/R project/AZE-analysis.R
+++ Assigment1/Code/R project/AZE-analysis.R
@@ -2,6 +2,8 @@
 
 
 # TASK 1 
+library(tseries)
+library(forecast)
 
 data.hungary.in = data.frame(hun = hungary_data.in, t = seq_along(hungary_data.in), seas.dummy = as.factor(cycle(hungary_data.in)))
 data.hungary.out = data.frame(hun = hungary_data.out, t = seq_along(hungary_data.out), seas.dummy = as.factor(cycle(hungary_data.out)))
@@ -9,34 +11,36 @@ data.hungary.out = data.frame(hun = hungary_data.out, t = seq_along(hungary_data
 
 plot(hungary_data.in)
 # We see that data have nice "inverse S shape", that is not typical for our general trend choices, that is why we will try
-# to catch the trend using polynomial of the third order.
+# to catch the trend using polynomial of the fourth order.
 
-model = lm(hun ~  + I(t^2) + I(t^3),data=data.hungary.in)
+model = lm(hun ~ t+  I(t^2) + I(t^3) + + I(t^4),data=data.hungary.in)
 
 
 plot(mod_data.in$hun, lwd = 2)
+
 lines(mod_data.in$t, predict(model), col = "red")
 
+summary(model)
+
 # maybe diagnostics?  plot(model), ale asi bych udelal az s hotovym modelem
-# summary(model)
 
 # We see that our model estimated the trend very nicely. We want to capture seasonality aswell. So we will try the dummy
 # variables approach
 
 
-model_s = lm(hun ~  + I(t^2) + I(t^3)+ seas.dummy, data= data.hungary.in)
+model1 = lm(hun ~ t + I(t^2) + I(t^3)+I(t^4)+ seas.dummy, data= data.hungary.in)
 
 
 par(mfrow = c(1,1))
 plot(hungary_data.in, lwd = 2)
-lines(ts(predict(model_s), start = start(hungary_data.in), frequency = frequency(hungary_data.in)), col = "red")
+lines(ts(predict(model1), start = start(hungary_data.in), frequency = frequency(hungary_data.in)), col = "red")
 
 
-plot(model_s)
-checkresiduals(model_s)
-# Model1 assumption E[eps] = 0 and var[eps] = sigma ^2 doesnt seem to be violated. 
+plot(model1)
+checkresiduals(model1)
 
-summary(model_s)
+#We see that our model violates the assumption of noncorellated errors,(E[eps] = 0 and var[eps] = sigma ^2 is good)
+
 
 #Lets check model performance 
 model.s.chars = matrix(NA, 1, 5)#7
@@ -53,38 +57,38 @@ model.s.chars[1,5] = mean(abs(residuals(model_s))) # MAPE in
 print(model.s.chars)
 
 #Task 3
-
+library(forecast)
 model2 = ets(hungary_data.in, model = "ZZZ", opt.crit = "lik", ic = "aic")
-summary(model2)
-par(mfrow = c(1,1))
-plot(model2)
-
-checkresiduals(model2)
-
-acf(residuals(model2)) # Autocorrelation function
-Box.test(residuals(model2), lag = floor(log(length(residuals(model2)))), type = c("Ljung-Box")) # Ljung - Box test
-
-acf(residuals(model2)^2) # Autocorrelation function RES^2
-Box.test(residuals(model2)^2, lag = floor(log(length(residuals(model2)))), type = c("Ljung-Box")) # Ljung - Box test RES^2
-
-jarque.bera.test(residuals(model2)) # Jarque - Bera test
-
-#Task 5 
-
-model3 = tbats(hungary_data.in)
-
-model3
-
+summary(model3)
 par(mfrow = c(1,1))
 plot(model3)
 
-
 checkresiduals(model3)
 
-acf(residuals(model3))
-Box.test(residuals(model03), lag = floor(log(length(residuals(model3)))), type = c("Ljung-Box"))
+acf(residuals(model3)) # Autocorrelation function
+Box.test(residuals(model3), lag = floor(log(length(residuals(model2)))), type = c("Ljung-Box")) # Ljung - Box test
 
 acf(residuals(model3)^2) # Autocorrelation function RES^2
-Box.test(residuals(model3)^2, lag = floor(log(length(residuals(model3)))), type = c("Ljung-Box"))
+Box.test(residuals(model3)^2, lag = floor(log(length(residuals(model2)))), type = c("Ljung-Box")) # Ljung - Box test RES^2
 
-jarque.bera.test(residuals(model3))
+jarque.bera.test(residuals(model3)) # Jarque - Bera test
+
+#Task 5 
+
+model5 = tbats(hungary_data.in)
+
+model5
+
+par(mfrow = c(1,1))
+plot(model5)
+
+
+checkresiduals(model5)
+
+acf(residuals(model5))
+Box.test(residuals(model5), lag = floor(log(length(residuals(model3)))), type = c("Ljung-Box"))
+
+acf(residuals(model5)^2) # Autocorrelation function RES^2
+Box.test(residuals(model5)^2, lag = floor(log(length(residuals(model3)))), type = c("Ljung-Box"))
+
+jarque.bera.test(residuals(model5))
