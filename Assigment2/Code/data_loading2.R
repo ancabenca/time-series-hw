101debf1b898067138985533d7a88ab5c2722788
DESKTOP-6VS722T\Uzivatel <benesova.work@gmail.com>
2024-05-08 11:11:04 +0200

    omg snad je to dobře

--- Assigment2/Code/data_loading2.R
+++ Assigment2/Code/data_loading2.R
@@ -19,7 +19,7 @@ library(tseries) #arima
 library(lmtest)
 library(car)
 library(forecast) #auto.arima
-library(rugarch)
+#library(rugarch)
 
 #------------------------------------------------------------------------------
 ##Data loading-------------------------------------------------------------------
@@ -32,6 +32,35 @@ getSymbols(ticker, src = "yahoo")
 
 # View loaded data
 View(NTDOY)
+summary(NTDOY)
 
-nintendo = NTDOY$NTDOY.Adjusted
+#Explanation of each column
+# NTDOY.Open: This column represents the opening price of Nintendo's stock on a 
+#particular trading day. The opening price is the price at which the first trade of the day occurred.
+
+# NTDOY.High: This column represents the highest price at which Nintendo's stock traded during the trading day.
+#It indicates the peak price reached by the stock at any point during the day's trading session.
+
+# NTDOY.Low: This column represents the lowest price at which Nintendo's stock traded during the trading day.
+#It indicates the lowest price reached by the stock at any point during the day's trading session.
+
+# NTDOY.Close: This column represents the closing price of Nintendo's stock on a particular trading day. 
+#As explained earlier, the closing price is the final price at which the stock traded before the market closed for the day.
+
+# NTDOY.Volume: This column represents the trading volume of Nintendo's stock on a particular trading day. 
+#Trading volume refers to the total number of shares that were traded during the day. It indicates the level of investor interest and activity in the stock.
+
+#NTDOY.Adjusted: This column likely represents the adjusted closing price of Nintendo's stock.
+#The adjusted closing price accounts for any corporate actions such as stock splits, dividends, or other adjustments that may affect the stock's price.
+#It provides a more accurate representation of the stock's performance over time, taking into account these factors
 #------------------------------------------------------------------------------
+#Time series format---------------------------------------------------------------
+
+nintendo<- NTDOY$NTDOY.Adjusted
+start(nintendo)
+end(nintendo)
+# Create a time series object
+nintendo_ts <- ts(nintendo, start = c(2007, 1,3), end = c(2024,5,7), frequency = 365)
+
+# Subset the time series to the desired time window (from January 2017 to March 2024)
+nintendo_ts.in <- window(nintendo_ts, start = c(2017, 1), end = c(2024, 3))
