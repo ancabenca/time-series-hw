c2657a29e2177daeb97b21d00b339c05c4e565bc
DESKTOP-6VS722T\Uzivatel <benesova.work@gmail.com>
2024-05-08 10:07:37 +0200

    init

--- Assigment2/Code/data_loading2.R
+++ Assigment2/Code/data_loading2.R
@@ -5,12 +5,21 @@
 #Identify, Estimate parameters, Verify, Plot and Discuss the estimated volatility.
 #Predict the vývoj of returns and volatility 5 days ahead
 
+
+
+##Cleaning----------------------------------------------------------------------
+rm(list = ls())
+graphics.off()
 #-------------------------------------------------------------------------------
 ##Libraries----------------------------------------------------------------------
 
 #install.packages("quantmod")
 library(quantmod)
-
+library(tseries) #arima
+library(lmtest)
+library(car)
+library(forecast) #auto.arima
+library(rugarch)
 
 #------------------------------------------------------------------------------
 ##Data loading-------------------------------------------------------------------
@@ -23,3 +32,5 @@ getSymbols(ticker, src = "yahoo")
 
 # View loaded data
 View(NTDOY)
+
+#------------------------------------------------------------------------------
